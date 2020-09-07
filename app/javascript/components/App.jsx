91335397f7accba92f1e1ea14f98fbf411279d41
saadatatta <muhammadsaadat42@gmail.com>
2020-09-07 19:43:38 +0500

    Added routes component to app.jsx

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import HomePage from "../pages/home-page/home-page.component";
+import Header from "./header/header.component";
 
 const App = () => {
     return (
-        <div>App component</div>
+        <Fragment>
+            <Header></Header>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={HomePage}></Route>
+                </Switch>
+            </Router>
+        </Fragment>
     )
 }
 
