e1e73164973abaa52a18f6f4b12f5b116be008d0
saadatatta <muhammadsaadat42@gmail.com>
2020-09-07 19:42:48 +0500

    Map JS routes to react in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'homepage/index'
+  namespace :api do
+    namespace :v1 do
+
+    end
+  end
+
+  # Redirect all requests which are not api to react frontend
+  get '/*path' => 'homepage#index'
   root 'homepage#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
