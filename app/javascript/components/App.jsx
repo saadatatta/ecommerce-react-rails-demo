3ec2f9b8762f28ced77ada4a3744fcb73cba743c
saadatatta <muhammadsaadat42@gmail.com>
2020-09-10 15:53:43 +0500

    Added sign up component route to App.jsx

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,14 +2,17 @@ import React, {Fragment} from 'react'
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import HomePage from "../pages/home-page/home-page.component";
 import Header from "./header/header.component";
+import SignUp from "./sign-up/sign-up.component";
 
 const App = () => {
     return (
         <Fragment>
-            <Header></Header>
+
             <Router>
+                <Header></Header>
                 <Switch>
                     <Route exact path="/" component={HomePage}></Route>
+                    <Route exact path="/sign_up" component={SignUp}></Route>
                 </Switch>
             </Router>
         </Fragment>
