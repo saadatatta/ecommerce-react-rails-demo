f3086fed644ea2f0bb67fe7e03193117d6302b69
saadatatta <muhammadsaadat42@gmail.com>
2020-09-14 12:58:13 +0500

    Added react redux boiler plate code

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
+import {Provider} from "react-redux"
+import store from "../redux/store"
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+      <Provider store={store}>
+        <App />
+      </Provider>
+    ,
     document.body.appendChild(document.createElement('div')),
   )
 })
---
+++ app/javascript/redux/root-reducer.js
@@ -0,0 +1,7 @@
+import {combineReducers} from "redux"
+
+const rootReducer = combineReducers({
+    currentUser: null
+})
+
+export default rootReducer
