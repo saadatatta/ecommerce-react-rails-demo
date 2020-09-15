39ac5c63934c733c468b38d3511eff1d30c4e288
saadatatta <muhammadsaadat42@gmail.com>
2020-09-15 14:52:20 +0500

    Added private route for homepage

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "../pages/home-page/home-page.component";
 import Header from "./header/header.component";
 import SignUp from "./sign-up/sign-up.component";
 import SignIn from "./sign-in/sign-in.component";
+import PrivateRoute from "./routing/private-route.component";
 
 const App = () => {
     return (
@@ -12,7 +13,7 @@ const App = () => {
             <Router>
                 <Header></Header>
                 <Switch>
-                    <Route exact path="/" component={HomePage}></Route>
+                    <PrivateRoute exact path="/" component={HomePage}></PrivateRoute>
                     <Route exact path="/sign_up" component={SignUp}></Route>
                     <Route exact path="/sign_in" component={SignIn}></Route>
                 </Switch>
