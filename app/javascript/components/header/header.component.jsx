455c9e69dd0b965204c28ca004ac3d0cb668f0e4
saadatatta <muhammadsaadat42@gmail.com>
2020-09-15 14:51:21 +0500

    Added logout link and action

--- app/javascript/components/header/header.component.jsx
+++ app/javascript/components/header/header.component.jsx
@@ -1,7 +1,23 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import {logOut} from "../../redux/auth/auth.actions";
+import axios from "axios"
+import {connect} from "react-redux";
+import {setAuthTokenHeaders} from "../../utils/axios";
 
-const Header = () => {
+const Header = ({logOutUser}) => {
+
+    const signOutUser = async () => {
+        try {
+            setAuthTokenHeaders()
+            await axios.delete("/api/v1/auth/sign_out.json")
+            sessionStorage.removeItem('user')
+            logOutUser()
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
+
     return (
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -18,16 +34,16 @@ const Header = () => {
             </div>
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="text-sm lg:flex-grow">
-                    <a href="#responsive-header"
-                       className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    <a
+                        className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                         Docs
                     </a>
-                    <a href="#responsive-header"
-                       className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    <a
+                        className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                         Examples
                     </a>
-                    <a href="#responsive-header"
-                       className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                    <a
+                        className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
                         Blog
                     </a>
                 </div>
@@ -38,10 +54,18 @@ const Header = () => {
                     <Link to="/sign_in"
                           className="inline-block text-sm pr-4 mr-2 py-2 text-white lg:mt-0">SIGN
                         IN</Link>
+                    <div onClick={signOutUser}
+                         className="inline-block text-sm pr-4 mr-2 py-2 text-white lg:mt-0 cursor-pointer">SIGN
+                        OUT
+                    </div>
                 </div>
             </div>
         </nav>
     )
 }
 
-export default Header
+const mapDispatchToProps = (dispatch) => ({
+    logOutUser: () => dispatch(logOut())
+})
+
+export default connect(null,mapDispatchToProps)(Header)
