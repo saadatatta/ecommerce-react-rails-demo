48ec212f9e70678c7d4a74b1e87946675965728b
saadatatta <muhammadsaadat42@gmail.com>
2020-09-15 17:11:38 +0500

    Added redux logger for debugging

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,6 +1,13 @@
-import {createStore} from "redux"
+import {applyMiddleware, createStore} from "redux"
 import rootReducer from "./root-reducer"
 
-const store = createStore(rootReducer)
+const middlewares = []
+
+if (process.env.NODE_ENV === "development") {
+    const {logger} = require(`redux-logger`);
+    middlewares.push(logger)
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export default store
