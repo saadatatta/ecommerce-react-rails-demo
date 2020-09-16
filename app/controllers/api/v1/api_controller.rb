6523590501e25cfa02af57846df5e9aa27489ff3
saadatatta <muhammadsaadat42@gmail.com>
2020-09-16 19:05:12 +0500

    current_user helper method added. Rescue from exceptions with custom essage

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,42 @@
-class ApiController < ApplicationController
-  include DeviseTokenAuth::Concerns::SetUserByToken
+module Api
+  module V1
+    class ApiController < ApplicationController
+
+      include DeviseTokenAuth::Concerns::SetUserByToken
+
+      helper_method :current_user
+
+      rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+      rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+      protected
+
+      def current_user
+        current_api_v1_user
+      end
+
+      private
+
+      def render_resource_error_message(resource, status= 422)
+        render json: {
+            errors: resource.errors.full_messages
+        }, status: status
+      end
+
+      def render_error_message(error='', status=422)
+        render json: {
+          error: error 
+        }, status: status
+      end
+
+      def render_unprocessable_entity_response(exception)
+        render_error_message( exception.record.errors)
+      end
+    
+      def render_not_found_response(exception)
+        render_error_message(exception.message)
+      end
+    end
+  end
 end
+
