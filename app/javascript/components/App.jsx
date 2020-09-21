eb5ed9fee3c4c121e46bde1e72471ef9b3a55598
saadatatta <muhammadsaadat42@gmail.com>
2020-09-21 14:16:16 +0500

    Added suspense component for lazy loading

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,16 +1,19 @@
-import React, {Fragment, useEffect} from 'react'
+import React, {Fragment, useEffect, Suspense, lazy} from 'react'
 import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom"
 import axios from "axios"
 import {connect} from "react-redux"
-import HomePage from "../pages/home-page/home-page.component";
 import Header from "./header/header.component";
-import SignUp from "./sign-up/sign-up.component";
-import SignIn from "./sign-in/sign-in.component";
 import PrivateRoute from "./routing/private-route.component";
 import {setAuthTokenHeaders} from "../utils/axios";
 import {authFailure, authSuccess} from "../redux/auth/auth.actions";
 import {constructAuthObject} from "../redux/auth/auth.utils";
 import AuthRedirectRoute from "./routing/auth-redirect-route.component";
+import Spinner from "./spinner/spinner.component";
+
+const SignUp = lazy(()=> import("./sign-up/sign-up.component"))
+const SignIn = lazy(()=> import("./sign-in/sign-in.component"))
+const HomePage = lazy(()=> import("../pages/home-page/home-page.component"))
+
 
 const App = ({setAuthSuccess, setAuthFailure, auth}) => {
     useEffect(() => {
@@ -22,7 +25,7 @@ const App = ({setAuthSuccess, setAuthFailure, auth}) => {
         try {
             setAuthTokenHeaders()
             const res = await axios.get('/api/v1/users/logged_in.json')
-            if(res.status === 200){
+            if (res.status === 200) {
                 setAuthSuccess(constructAuthObject(res))
             }
         } catch (e) {
@@ -36,10 +39,13 @@ const App = ({setAuthSuccess, setAuthFailure, auth}) => {
             <Router>
                 <Header/>
                 <Switch>
-                    <PrivateRoute exact path="/" component={HomePage}/>
-                    <AuthRedirectRoute exact path="/sign_up" component={SignUp}/>
-                    <AuthRedirectRoute exact path="/sign_in" component={SignIn}/>
+                    <Suspense fallback={<Spinner/>}>
+                        <PrivateRoute exact path="/" component={HomePage}/>
+                        <AuthRedirectRoute exact path="/sign_up" component={SignUp}/>
+                        <AuthRedirectRoute exact path="/sign_in" component={SignIn}/>
+                    </Suspense>
                 </Switch>
+
             </Router>
         </Fragment>
     )
