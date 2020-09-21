09aee8d58c50c3d954de63d6c6322f319de35087
saadatatta <muhammadsaadat42@gmail.com>
2020-09-21 14:25:45 +0500

    Changed spinner component

--- app/javascript/components/spinner/spinner.component.jsx
+++ app/javascript/components/spinner/spinner.component.jsx
@@ -1,18 +1,10 @@
 import React from "react"
-import Loader from 'react-loader-spinner'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import "./spinner.styles.scss"
 
 const Spinner = () => {
     return (
         <div className="spinner-overlay">
-            <Loader
-                type="TailSpin"
-                color="#00BFFF"
-                height={100}
-                width={100}
-                timeout={3000}
-            />
+            <div className="spinner-container"></div>
         </div>
     );
 }
