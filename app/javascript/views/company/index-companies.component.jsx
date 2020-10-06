52c816f540eedb05ad0f7bb914f6ce44475ae439
saadatatta <muhammadsaadat42@gmail.com>
2020-10-06 19:02:15 +0500

    Render company index page

--- app/javascript/views/company/index-companies.component.jsx
+++ app/javascript/views/company/index-companies.component.jsx
@@ -1,8 +1,47 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import {setAuthTokenHeaders} from "../../utils/axios";
+import axios from "axios"
+import {reactToastError} from "../../utils/toast";
+import IndexCompanyCard from "./index-company-card.component";
+import {v4 as uuidv4} from "uuid";
 
 const IndexCompanies = () => {
+    const [companies, setCompanies] = useState([])
+
+    useEffect(() => {
+        async function fetchCompaniesAsync() {
+            try {
+                setAuthTokenHeaders()
+                const result = await axios.get("/api/v1/companies.json")
+                const companies = result.data
+                if (companies && companies.length > 0) {
+                    setCompanies(companies)
+                }
+            } catch (e) {
+                e.response.data.errors.forEach(error => reactToastError(error))
+            }
+        }
+
+        fetchCompaniesAsync()
+    }, [])
+
     return (
-        <div>Index Companies</div>
+        <>
+            <div>Index Companies</div>
+            {
+                companies.length === 0 ?
+                    <div> No Companies found </div> :
+                    <div className="flex m-8">
+                        {
+                            companies.map((company) => {
+                                return (
+                                    <IndexCompanyCard company={company} key={uuidv4()}/>
+                                )
+                            })
+                        }
+                    </div>
+            }
+        </>
     )
 }
 
