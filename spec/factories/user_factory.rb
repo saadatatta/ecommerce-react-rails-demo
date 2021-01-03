8a28d7b647f51e1f4532fc8e223b286f9daa31f8
saadatatta <muhammadsaadat42@gmail.com>
2021-01-03 10:59:57 +0500

    Create companies on user create

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+    confirmed_at { Time.now }
+    factory :user_with_companies do
+      after(:create) do |user|
+        create_list :company, 3, user_id: user.id
+      end
+    end
   end
 end
